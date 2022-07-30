e63e2d7b8f01297d06cd3570385a0a9d5b019764
vuvuzelaparty <vuvu@posteo.net>
2022-07-30 11:09:46 -0400

    cleaned up some of the code

--- longCompSeq.c
+++ longCompSeq.c
@@ -7,37 +7,35 @@ int isPrime(int num);
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		printf("Please enter a single number higher than 3\n");
-		exit(0);
+		return 1;
 	}
-	int count = 0, max = 0, maxVal = 4, limit = atoi(*(++argv));
+	int count = 0, maxLength = 0, maxVal = 4, limit = atoi(*(++argv));
 	if (limit < maxVal) {
 		printf("Please enter a single number higher than 3\n");
-		exit(0);
+		return 1;
 	}
 	for (int i = maxVal; i <= limit; i++) {
 		if (isPrime(i))
 			count = 0;
 		else {
 			count++;
-			if (max < count) {
-				max = count;
-				maxVal = i-max+1;
+			if (maxLength < count) {
+				maxLength = count;
+				maxVal = i-maxLength+1;
 			}
 		}
 	}
-	printf("%d \n", max);
-	while (max--)
+	printf("%d \n", maxLength);
+	while (maxLength--)
 		printf("%d ", maxVal++);
 	printf("\n");
 	return 0;
 }
 
 int isPrime(int num) {
-	int ret = 0;
-	for (int i = 2; i <= sqrt(num); i++) // sqrt to reduce time, smallest factor has to be less than this
-		if (num % i == 0) {
-			ret++;
-			break;
-		}
-	return !ret;
+	int limit = sqrt(num);
+	for (int i = 2; i <= limit; i++) // sqrt to reduce time, smallest factor has to be less than this
+		if (num % i == 0)
+			return 0;
+	return 1;
 }
